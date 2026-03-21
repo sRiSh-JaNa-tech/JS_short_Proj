7005f37c9affc7f0d0d96fa7f6e08c5ae6445159
Srish Jana <srishjana@gmail.com>
2026-03-21 22:50:04 +0530

    New GraphQL project

---
+++ graphql_demo/server/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@as-integrations/express4");
+const { ApolloServerPluginLandingPageLocalDefault } = require("@apollo/server/plugin/landingPage/default");
+
+async function startServer() {
+  const app = express();
+
+  const typeDefs = `
+    type Todo {
+      id: ID!
+      title: String!
+      completed: Boolean
+    }
+
+    type Query {
+      getTodos: [Todo]
+    }
+  `;
+
+  const resolvers = {
+    Query: {
+      getTodos: () => [
+        { id: "1", title: "Learn GraphQL", completed: false },
+        { id: "2", title: "Build Project", completed: true },
+      ],
+    },
+  };
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginLandingPageLocalDefault()],
+  });
+
+  await server.start();
+
+  app.use(cors());
+  app.use(
+    "/graphql",
+    express.json(),
+    expressMiddleware(server)
+  );
+
+  app.listen(8000, () => {
+    console.log("Server running at http://localhost:8000/graphql");
+  });
+}
+
+startServer();
